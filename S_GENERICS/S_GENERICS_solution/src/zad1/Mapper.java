c5352a6454107ed64a1e9c553b88356c34bad46d
Aleksandra <133956347+alessandra3747@users.noreply.github.com>
2025-02-23 11:28:20 +0100

    Add files via upload

---
+++ S_GENERICS/S_GENERICS_solution/src/zad1/Mapper.java
@@ -0,0 +1,14 @@
+/**
+ *
+ *  @author Fus Aleksandra S30395
+ *
+ */
+
+package zad1;
+
+
+public interface Mapper <T, R> {
+
+    public R map(T arg);
+
+}  
---
+++ S_GENERICS/S_GENERICS_solution/src/zad1/Selector.java
@@ -0,0 +1,14 @@
+/**
+ *
+ *  @author Fus Aleksandra S30395
+ *
+ */
+
+package zad1;
+
+
+public interface Selector <T> {
+
+    public boolean select(T arg);
+
+}  
